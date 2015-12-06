b1408feba5c96e1f08f26081a95db951b3049ca8
tmorita <graboros.dev@gmail.com>
2015-12-06 14:01:35 +0900

    ログイン周りの自動リダイレクト処理を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
+  before_filter :authenticate_user!, except: [:index]
+  before_action :redirect_to_timeline, only: [:index]
+
   def index
   end
 
   def timeline
+  end
+
+private
+  def redirect_to_timeline
+    redirect_to timeline_url if user_signed_in?
   end
 end
