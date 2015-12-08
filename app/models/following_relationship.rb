a24f0682c6729b064459a87895ec3623cbca1b8c
tmorita <graboros.dev@gmail.com>
2015-12-08 22:50:24 +0900

    ユーザ毎のページを作成

--- app/models/following_relationship.rb
+++ app/models/following_relationship.rb
@@ -1,4 +1,4 @@
 class FollowingRelationship < ActiveRecord::Base
-  belongs_to :following, class_name: "User", foreign_key: "followed_id"
-  belongs_to :follower, class_name: "User", foreign_key: "following_id"
+  belongs_to :following, class_name: "User", foreign_key: "following_id"
+  belongs_to :follower, class_name: "User", foreign_key: "followed_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,15 @@ class User < ActiveRecord::Base
   has_many :like_tweets, through: :likes
 
   has_many :followed_relationships, class_name: "FollowingRelationship", foreign_key: "followed_id"
-  has_many :followers, through: :followed_relationships
+  has_many :followers, through: :followed_relationships, source: :following
   has_many :following_relationships, class_name: "FollowingRelationship", foreign_key: "following_id"
-  has_many :followings, through: :following_relationships
+  has_many :followings, through: :following_relationships, source: :follower
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def timeline_tweets
-    timeline_users = self.followings << self
+    timeline_users = self.followings.to_a << self
     Tweet.where(user: timeline_users).order(updated_at: :desc)
   end
 end
