b5884b89eb87f88c5a7bc4b876cbee8b35e0e5df
tmorita <graboros.dev@gmail.com>
2015-12-14 01:06:28 +0900

    自分のツイートにはリツイートボタンを表示しないようにした

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,14 @@
 module UsersHelper
   def user_show_path?
-    request.path_info == user_path(current_user)
+    request.path_info == user_path(@user)
   end
   def user_followings_path?
-    request.path_info == user_followings_path(current_user)
+    request.path_info == user_followings_path(@user)
   end
   def user_followers_path?
-    request.path_info == user_followers_path(current_user)
+    request.path_info == user_followers_path(@user)
   end
   def user_likes_path?
-    request.path_info == user_likes_path(current_user)
+    request.path_info == user_likes_path(@user)
   end
 end
