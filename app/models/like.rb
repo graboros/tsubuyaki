8d5e922a3399e920babbd23f73d84028de4b0858
tmorita <graboros.dev@gmail.com>
2015-12-24 23:37:35 +0900

    特定のツイートに対して同じユーザが1回のみlikeできるようにModelにvalidatesを追加

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,5 @@
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :like_tweet, class_name: "Tweet", foreign_key: "tweet_id"
+  validates :tweet_id, uniqueness: { scope: :user_id }
 end
