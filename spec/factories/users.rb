02dc3b9667b9b779571f54c11dcfd58ed9d52c3e
tmorita <graboros.dev@gmail.com>
2015-12-29 00:38:54 +0900

    Userモデルの単体テストを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,18 @@
 
 FactoryGirl.define do
   factory :user do
+
+    factory :user1 do
+      email "takashi2003_jp@yahoo.co.jp"
+      username "takashi"
+      password "password"
+    end
+
+    factory :user2 do
+      email "graboros.dev@gmail.com"
+      username "graboros"
+      password "password"
+    end
+
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with username, password" do
+    user = build(:user1, username: "takashi", password: "password")
+    expect(user).to be_valid
+  end
+
+  it "is invalid without username" do
+    user = build(:user1, username: nil)
+    user.valid?
+    expect(user.errors[:username]).to include("can't be blank")
+  end
+
+  it "is invalid without password" do
+    user = build(:user1, password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+  end
+
+  it "is invalid with 2 characters username" do
+    user = build(:user1, username: "un")
+    user.valid?
+    expect(user.errors[:username]).to include("is too short (minimum is 3 characters)")
+  end
+
+  it "is valid with 3 characters username" do
+    user = build(:user1, username: "unm")
+    expect(user).to be_valid
+  end
+
+  it "is invalid when the username was taken" do
+    create(:user1, username: "takashi")
+    user = build(:user2,  username: "takashi")
+    user.valid?
+    expect(user.errors[:username]).to include("has already been taken")
+  end
+
+  it "is invalid when the email was taken" do
+    create(:user1, email: "takashi2003_jp@yahoo.co.jp" )
+    user = build(:user2, email: "takashi2003_jp@yahoo.co.jp")
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
 end
