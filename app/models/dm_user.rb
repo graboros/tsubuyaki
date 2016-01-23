4952056eee60a867eefe30323244c59719ac2093
tmorita <graboros.dev@gmail.com>
2016-01-24 01:52:58 +0900

    テストとビューを少し修正（＋消し忘れていたビューテンプレートがあったので削除）

--- app/models/dm_user.rb
+++ app/models/dm_user.rb
@@ -1,4 +1,5 @@
 class DmUser < ActiveRecord::Base
   belongs_to :dm
   belongs_to :user
+  validates :user, presence: true
 end
--- app/models/dmmessage.rb
+++ app/models/dmmessage.rb
@@ -2,5 +2,6 @@ class Dmmessage < ActiveRecord::Base
   belongs_to :dm, touch: true
   belongs_to :user
   validates :content, presence: true, length: { maximum: 140 }
+  validates :user, presence: true
   default_scope { order('created_at DESC') }
 end
